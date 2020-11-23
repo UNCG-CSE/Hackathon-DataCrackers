2bec3e1c7c6448d38fe0e3dcc87d62774cb2eadb
tsadhana9 <tsadhana9@gmail.com>
2020-11-23 12:59:56 -0500

    doc template added

--- src/app.py
+++ src/app.py
@@ -3,6 +3,8 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
+# Importing Libraries
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
